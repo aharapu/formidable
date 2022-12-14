ce77f3887bf42fd08ae976ef540b51454991f649
Valentin Harapu <aharapu2@yahoo.com>
2022-12-14 17:33:33 +0200

    add tech guidance and deps to clipboard

--- src/components/FeaturePreview/FeaturePreview.jsx
+++ src/components/FeaturePreview/FeaturePreview.jsx
@@ -38,133 +38,9 @@ export default function FeaturePreview() {
   const featureTestInstructions = useRecoilValue(featureTestInstruct);
   const automation = useRecoilValue(featureRequireAutomationTest);
 
-  const handleCopyClickOld = (evt) => {
-    var type = "text/html";
-    var blob = new Blob([clipboardContent], { type });
-    var data = [new ClipboardItem({ [type]: blob })];
-
-    navigator.clipboard.write(data).then(
-      function () {
-        /* success */
-        alert("copied!");
-      },
-      function () {
-        alert("error!");
-        /* failure */
-      }
-    );
-  };
-
   const handleCopyClick = () => {
-    updateClipboard({ what, criterias });
+    updateClipboard({ what, criterias, techGuidance, dependencies });
   };
-
-  const arrayToUL = (arr) => {
-    const ul = document.createElement("ul");
-    ul.append(
-      ...arr.map((value) => {
-        if (Array.isArray(value)) return arrayToUL(value);
-        const li = document.createElement("li");
-        li.textContent = value;
-        li.style = `font-family: 'Source Sans Pro'; padding-left: 16px; color: ${DARK_GREY}`;
-        return li;
-      })
-    );
-    return ul;
-  };
-
-  const displayTestInstructions = (testingInstructions) => {
-    let displayText = "<ul>";
-    testingInstructions.map((testInstruc) => {
-      displayText += "<li>" + testInstruc.scenarioName;
-      const givenValues = testInstruc.given.map((elem) => elem.value);
-      const whenValues = testInstruc.when.map((elem) => elem.value);
-      const thenValues = testInstruc.then.map((elem) => elem.value);
-      displayText +=
-        "<ul><li><strong> Given </strong>" +
-        givenValues.join(" <strong> and </strong>") +
-        "</li>";
-      displayText +=
-        "<li><strong> Then </strong>" +
-        thenValues.join("<strong> and </strong>") +
-        "</li>";
-      displayText +=
-        "<li><strong> When </strong>" +
-        whenValues.join("<strong> and </strong>") +
-        "</li></ul>";
-    });
-    displayText += "</ul>";
-    return displayText;
-  };
-
-  const criteriasList =
-    criterias.length > 0
-      ? arrayToUL(criterias.map((criteria) => criteria.value)).innerHTML
-      : "";
-  const techGuide =
-    techGuidance.length > 0 ? arrayToUL([techGuidance]).innerHTML : "";
-  const deps =
-    dependencies.length > 0
-      ? arrayToUL(dependencies.map((dep) => dep.value)).innerHTML
-      : '<span style="color: #97A0AF"><strong> NO </strong></span>';
-  const requiresFF = FF ? `YES - ${FF}` : " NO ";
-  const impactedProjects = arrayToUL(
-    impactedProj.map((proj) => proj.value)
-  ).innerHTML;
-  const requiresEdition =
-    edition.length > 0
-      ? arrayToUL(edition.map((ed) => ed.value)).innerHTML
-      : `<strong style="font-size: 20px; color: ${DARK_GREY}"> NO </strong>`;
-  const testInstructions =
-    featureTestInstructions.length > 0 &&
-    featureTestInstructions[0].scenarioName
-      ? displayTestInstructions(featureTestInstructions)
-      : "";
-  const requiresAutomation = automation ? " YES " : " NO ";
-  let clipboardContent = `
-        <span style="color:#97A0AF; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-          WHAT
-        </span>
-        <br />
-        <span style="font-family: 'Source Sans Pro'">${what}</span>
-       </br>
-        <span style="color:#6554C0; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           ACCEPTANCE CRITERIA
-        </span>
-        ${criteriasList}
-        </br>
-        <span style="color:#36B37E; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           TECHNICAL GUIDANCE
-        </span>
-       ${techGuide}
-       </br>
-         <span style="color:#4C9AFF; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           DEPENDENCIES
-        </span>
-       ${deps}
-       </br>
-        <span style="color:#FF991F; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           FEATURE FLAG: <strong style="font-size: 20px; color: ${DARK_GREY}">${requiresFF}</strong>
-        </span>
-      <br/>
-        <span style="color:#BF2600; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           IMPACTED PROJECTS
-        </span>
-       ${impactedProjects}
-       </br>
-        <span style="color:#FF5630; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-          REQUIRES AN EDITION: ${requiresEdition}
-        </span>
-       </br>
-        <span style="color:#008DA6; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           TESTING SCENARIOS
-        </span>
-       ${testInstructions}
-       </br>
-       <span style="color:#97A0AF; width: 407px; height: 19px; left:1067px; top: 371px;font-family: 'Source Sans Pro'; display: inline-block;margin: 10px 0; font-weight:600; font-size: 16px;">
-           REQUIRES AUTOMATION TESTING: <strong style="font-size: 20px; color: ${DARK_GREY}"> ${requiresAutomation}</strong>
-        </span>
-        `;
 
   // TODO -> use state to create an elaborate preview with colors and such
   return (
@@ -365,13 +241,25 @@ function updateClipboard({
   automation,
 }) {
   const criteriaValues = getValues(criterias);
+  const depValues = getValues(dependencies);
 
   const cc = new ClipboardContent();
 
-  cc.addH3({ content: "What:" })
+  cc.addH3({ content: "What:", color: LIGHT_GRAY })
     .addP({ content: what })
     .addH3({ content: "Acceptance Criteria:", color: PURPLE })
     .addList(criteriaValues);
+
+  // TODO -> trim and capitalize?
+  if (techGuidance) {
+    cc.addH3({ content: "Technical Guidance:", color: GREEN }).addP({
+      content: techGuidance,
+    });
+  }
+
+  if (dependencies.length > 0) {
+    cc.addH3({ content: "Dependencies:", color: BLUE }).addList(depValues);
+  }
 
   const content = cc.getContent();
 
