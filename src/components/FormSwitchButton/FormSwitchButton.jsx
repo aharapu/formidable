c51ab4f6f7cf87ea6e257a85521f9671e03fac3f
Valentin Harapu <aharapu2@yahoo.com>
2022-12-26 12:21:38 +0200

    remove bad prop error

--- src/components/FormSwitchButton/FormSwitchButton.jsx
+++ src/components/FormSwitchButton/FormSwitchButton.jsx
@@ -4,45 +4,53 @@ import { FormControlLabel, Grid, Switch } from '@mui/material';
 import styled from '@emotion/styled';
 
 // TODO -> make highlight on focus a lot more visible
-const StyledSwitch = styled(Switch)(({ checkedColor }) => {
-    return {
-        '& .MuiSwitch-switchBase': {
-            '&.Mui-checked': {
-                color: checkedColor,
+const StyledSwitch = styled(
+    Switch,
+    {
+        shouldForwardProp: (prop) => prop !== 'checkedColor',
+    },
+)(
+    ({ checkedColor }) => {
+        return {
+            '& .MuiSwitch-switchBase': {
+                '&.Mui-checked': {
+                    color: checkedColor,
                 // },
                 // '& + .MuiSwitch-track': {
                 //     opacity: 1,
                 //     backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
                 // },
+                },
             },
-        },
-        '&.Mui-checked': { // not it
+            '&.Mui-checked': { // not it
             // color: 'black',
             // backgroundColor: checkedColor,
-        },
-        '& .Mui-checked + .MuiSwitch-track': {
+            },
+            '& .Mui-checked + .MuiSwitch-track': {
             // backgroundColor: checkedColor,
-        },
-        '& .Mui-checked + .MuiSwitch-thumb': { // not it
-        // backgroundColor: 'red',
+            },
+            '& .Mui-checked + .MuiSwitch-thumb': { // not it
+                // backgroundColor: 'red',
             // color: checkedColor,
-        },
-        '& .MuiSwitch-thumb': {
+            },
+            '& .MuiSwitch-thumb': {
             // color: checkedColor, // kinda does it, but can't change the ripple
             // backgroundColor: 'purple',
-        },
-        // TODO -> can these be used in theme?
-        '& .MuiTocuhRipple-root': {
+            },
+            // TODO -> can these be used in theme?
+            '& .MuiTocuhRipple-root': {
             // color: RED,
-        },
-        '&.MuiTouchRipple-ripple': {
+            },
+            '&.MuiTouchRipple-ripple': {
             // opacity: 1,
-        },
-        '&.MuiTouchRipple-rippleVisible': {
+            },
+            '&.MuiTouchRipple-rippleVisible': {
             // opacity: 1,
-        },
-    };
-});
+            },
+        };
+    },
+);
+
 export function FormSwitchButton({ value, onChange, label, checkedColor }) {
     return (
         <Grid item xs={12} style={{}}>
