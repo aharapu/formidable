c718ef443a43da14441166e7e488e3971376d85f
Valentin Harapu <aharapu2@yahoo.com>
2023-01-05 22:08:39 +0200

    use firestore get and set

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,8 +1,13 @@
 import { Button } from '@mui/material';
 import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
+
+import { doc, setDoc, getDoc  } from 'firebase/firestore';
+
 import { authClient } from '../../auth/auth';
 import { userAtom } from '../../recoil/atoms/user';
+import { db } from '../../firebase/app';
+
 import { FFTextField } from '../mui-wrappers/FFTextField/FFTextField';
 
 export default function PageHeader() {
@@ -21,10 +26,52 @@ export default function PageHeader() {
             .then((userCredential) => {
             // Signed in
                 const user = userCredential.user;
-                console.log('user', user);
+                console.log('signed up user:', user);
                 setUser({ displayName: user.displayName, email: user.email, uid: user.uid });
+                return setDoc(doc(db, 'users', user.uid), {
+                    displayName: user.displayName,
+                    email: user.email,
+                    uid: user.uid,
+                });
+            }).then(result => {
+                console.log('set doc result', result);
             })
             .catch(console.error);
+    };
+
+    const handleGet = async () => {
+        try {
+            console.log('get');
+            const docRef = doc(db, 'users', user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (docSnap.exists()) {
+                console.log('User data:', docSnap.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log('No user document!');
+            }
+
+            const docRef2 = doc(db, 'inputs', 'TMDJVUuJSLkhP8QvzIZ1');
+            const docSnap2 = await getDoc(docRef2);
+
+            if (docSnap2.exists()) {
+                console.log('Input data:', docSnap2.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log('No input document!');
+            }
+
+            // const userReference = docSnap2.data().user;
+            // const docSnap3 = await getDoc(userReference);
+
+            // console.log('user through refernce', docSnap3.data());
+        } catch(e) {
+            console.log('GET ERROR');
+            console.error(e);
+        }
+
+
     };
 
     return (
@@ -80,6 +127,13 @@ export default function PageHeader() {
                 size="small"
             >
                 Sign Out
+            </Button>
+            <Button
+                variant="contained"
+                onClick={handleGet}
+                size="small"
+            >
+                GET
             </Button>
         </div>
     );
